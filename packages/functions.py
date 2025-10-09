fedea130d4e99d4a255da2c697264af72965062d
Coulibaly Cheick Oumar <cheick@MacBook-Pro-de-Coulibaly.local>
2025-10-09 12:18:32 +0200

    -Add age and revenu categorize function and feature engenering

--- packages/functions.py
+++ packages/functions.py
@@ -43,6 +43,46 @@ def category_descriptive_stat(df, columns):
         print(f"Pour la colonne {column}")
         elt = pd.DataFrame({
             "Effectif": df[column].value_counts(dropna=False),
-            "Fréquence":  df[column].value_counts(normalize=True, dropna=False)*100
+            "Fréquence":  (df[column].value_counts(normalize=True, dropna=False)*100).round(2)
         })
         display(elt)
+
+def categorize_age(age: int):
+    """
+        Fonction qui permet de catgéoriser une personne dans une tranche d'age en fonction de son age
+
+        ----------
+        parameters:
+        age: int
+            age de l'individu
+
+        returns
+            category: string
+                tranche d'age de l'individu
+    """
+    if age <= 30:
+        return "Jeune"
+    elif age > 30 and age < 43:
+        return "Adulte"
+    else:
+        return "Senior"
+
+def categorize_revenu(revenu: float):
+    """
+        Fonction qui permet de catégoriser les revenus d'un employé en fonction du montant
+
+        ----------
+        parameters:
+        revenu: float
+            revenu mensuel de l'employé
+        returns
+            category: string
+                catégorie à laquelle appartient le revenu
+    """
+
+    if revenu <= 2911:
+        return "Bas"
+    elif revenu > 2911 and revenu < 8379:
+        return "Moyen"
+    else:
+        return "Élevé"
